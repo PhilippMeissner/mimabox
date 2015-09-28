3939aa463ee8bb58096ca7bf39356516ecfbcda4
Philipp Meißner <pm@zeit.io>
2015-09-28 12:12:06 +0200

    added help to show all available commands

--- app/controllers/mpd_controller.rb
+++ app/controllers/mpd_controller.rb
@@ -1,4 +1,11 @@
 class MpdController < ApplicationController
+  require 'ruby-mpd'
+
+  # List all available commands for an MPD instance
   def help
+    mpd = MPD.new
+    # Sort ASC
+    @methods = mpd.methods.sort_by{|m| m}
   end
+
 end
