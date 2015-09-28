33f0e9c2f7e551a22cc75aedd0ca48974be6fffe
Philipp Meißner <pm@zeit.io>
2015-09-28 15:13:28 +0200

    addded routes for MpdController

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
-  get 'mpd/help'
+
+  namespace :mpd do
+    get 'help' #==> get "mpd/help", as: :mpd_help
+    get 'status'
+    get 'start'
+    get 'stop'
+    get 'next'
+    get 'previous'
+    get 'pause'
+  end
+
 
   match '*path' => 'static#home', via: [:get, :post]
   root 'static#home'
