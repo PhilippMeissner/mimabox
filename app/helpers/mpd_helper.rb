e88f63fb40137dc9c5361a390a2851331a1e5613
meLog <r.fastenrath@gmail.com>
2015-09-30 15:57:59 +0200

    changed player controlls to post

--- app/helpers/mpd_helper.rb
+++ app/helpers/mpd_helper.rb
@@ -1,5 +1,5 @@
 module MpdHelper
   def link_to_glyph(text, link, icon)
-    link_to content_tag(:span, " #{text}", class: "btn btn-primary glyphicon glyphicon-#{icon}"), link
+    link_to content_tag(:span, " #{text}", class: "btn btn-primary glyphicon glyphicon-#{icon}"), link, method: :post
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
     get 'help' #==> get "mpd/help", as: :mpd_help
     get 'status'
     post 'start'
-    get 'stop'
-    get 'next'
-    get 'previous'
-    get 'pause'
-    get 'update'
-    get 'add_song'
+    post 'stop'
+    post 'next'
+    post 'previous'
+    post 'pause'
+    post 'update'
+    post 'add_song'
   end
 
   root 'static#home'
