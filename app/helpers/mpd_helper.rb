543baf96b9552d8980943f6a3688476decd2019e
Philipp Meißner <pm@zeit.io>
2015-10-01 15:10:53 +0200

    fixed class getting assigned to wrong element

--- app/helpers/mpd_helper.rb
+++ app/helpers/mpd_helper.rb
@@ -1,5 +1,5 @@
 module MpdHelper
   def link_to_glyph(text, link, icon)
-    link_to content_tag(:span, " #{text}", class: "btn btn-primary glyphicon glyphicon-#{icon}"), link, method: :post
+    link_to content_tag(:span, " #{text}", class: "btn btn-primary glyphicon glyphicon-#{icon}"), link, method: :post, class: "ctrl"
   end
 end
