eac9e44836b9857f59ce9cc2f228520c139bc00d
Philipp Meißner <pm@zeit.io>
2015-10-02 13:14:03 +0200

    added route to play a certain song

--- config/routes.rb
+++ config/routes.rb
@@ -9,8 +9,11 @@ Rails.application.routes.draw do
     post 'previous'
     post 'pause'
     post 'update'
-    post 'add_song'
   end
+  # For some reason this path must not be in the namespace?!
+  post '/mpd/play_song/:id', to: 'mpd#play_song', as: 'mpd_play_song'
+
+
 
   root 'static#home'
 
